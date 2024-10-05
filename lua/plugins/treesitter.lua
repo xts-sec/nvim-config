4da9d0630c797200e3ce324a376c0d41d95a5f34
xts-sec <30200315+xts-sec@users.noreply.github.com>
2024-10-05 23:26:26 +0100

    working, modular config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "lua",
+                "c"
+            },
+            sync_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+-- tab width
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- line numbers
+vim.cmd("set number")
+
+-- leader key
+vim.g.mapleader = " "
+
