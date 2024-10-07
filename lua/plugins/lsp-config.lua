d44652e5f3ea0082853dcef9f5dac2ffc140bc79
xts-sec <30200315+xts-sec@users.noreply.github.com>
2024-10-07 07:57:04 +0100

    added lsp action for references

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,32 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({ capabilities = capabilities })      -- lua
-            lspconfig.clangd.setup({ capabilities = capabilities })      -- c
-            lspconfig.marksman.setup({ capabilities = capabilities })    -- markdown
-            lspconfig.jedi_language_server.setup({ capabilities = capabilities }) -- python
+			lspconfig.lua_ls.setup({ capabilities = capabilities }) -- lua
+			lspconfig.clangd.setup({ capabilities = capabilities }) -- c
+			lspconfig.marksman.setup({ capabilities = capabilities }) -- markdown
+			lspconfig.jedi_language_server.setup({ capabilities = capabilities }) -- python
 
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
